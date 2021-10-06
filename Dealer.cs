e02d77b4dfa8eb1805955d67b592558e95cb1fa7
NicholasBoss <nicholasboss0914@gmail.com>
2021-10-06 12:25:31 -0600

    Created base Dealer.cs file

---
+++ Dealer.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace cse210_tc04
+{
+    class Dealer
+    {
+        public void IsFirstTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetDeal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckGuess()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PointsPerCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayCards()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
