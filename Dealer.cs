6e3cbeaa6772d571ca068ab22a28e386d2518b59
SilverKnight317 <ajclarkson3@gmail.com>
2021-10-07 18:33:09 -0600

    added functions and such to CheckGuess() and CheckScore()

--- Dealer.cs
+++ Dealer.cs
@@ -5,69 +5,104 @@ namespace cse210_tc04
 {   
 
     /// <summary>
-    /// The Dealer is responsible for managing the deals which include: 
+    /// The dealer is responsible for managing the deals which include: 
     /// Showing the card
     /// managing point assignment
-    /// </summary>
+    
+    /// <summary>
     class Dealer
     {
         ///<summary>
         /// Member variables go here:
-        ///</summary>
+        ///<summary>
         const int NUM_CARDS = 13;
         List<int> _cards = new List<int>();
         public int _numDeals;
+        public string _guess;
+        int _current_card_number;
+        bool _increase_score;
 
         ///<summary>
-        /// Checks to see if it is the first turn.
-        ///</summary>
+        ///<summary>
         public bool IsFirstTurn()
         {
             return _numDeals == 0;
         }
 
         ///<summary>
-        /// The ResetDeal method handles the clearing of the deal of cards and
-        /// generates the next card in the deal.
-        ///</summary>
+        ///<summary>
         public void ResetDeal()
         {
             _numDeals++;
             _cards.Clear();
             Random randomGenerator = new Random();
-
+            
             for (int i = 0; i < NUM_CARDS; i++)
             {
                 int card = randomGenerator.Next(1,14);
                 _cards.Add(card);
             }
-            
-
         }
 
         ///<summary>
-        ///</summary>
+        ///<summary>
         public void CheckGuess()
         {
+            int _numguess;
+            if (_guess == "l")
+            {
+                _numguess = 0;
+                if (_numguess < _current_card_number)
+                {
+                    _increase_score = true;
+                    CheckScore();
+                }
+                if (_numguess > _current_card_number)
+                {
+                    _increase_score = false;
+                    CheckScore();
+                }
+            }
+            if (_guess == "h")
+            {
+                _numguess = 15;
+                if (_numguess > _current_card_number)
+                {
+                    _increase_score = true;
+                    CheckScore();
+                }
+                if (_numguess < _current_card_number)
+                {
+                    _increase_score = false;
+                    CheckScore();
+                }
+            }
             throw new NotImplementedException();
         }
-
+    
         ///<summary>
-        ///</summary>
+        ///<summary>
         public void CheckScore()
         {
-            throw new NotImplementedException();
+            if (_increase_score == true)
+            {
+                _score += 100;
+            }
+            if (_increase_score == false)
+            {
+                _score = _score - 75;
+            }
         }
 
         ///<summary>
-        ///</summary>
+        ///<summary>
         public void PointsPerCard()
         {
             throw new NotImplementedException();
         }
 
         ///<summary>
-        ///</summary>
+        ///<summary>
         public void DisplayCards()
         {
             throw new NotImplementedException();
