547183b031ddfef4dcb8afe31e7bde920a3d191f
Aoi Kuriki <aoi727noel@gmail.com>
2021-10-09 16:04:15 -0600

    Aoi made PointsPerCard and DisplayCard

--- Dealer.cs
+++ Dealer.cs
@@ -8,46 +8,46 @@ namespace cse210_tc04
     /// The dealer is responsible for managing the deals which include: 
     /// Showing the card
     /// managing point assignment
-    
-    /// <summary>
+    /// </summary>
     class Dealer
     {
         ///<summary>
         /// Member variables go here:
-        ///<summary>
+        ///</summary>
         const int NUM_CARDS = 13;
         List<int> _cards = new List<int>();
         public int _numDeals;
         public string _guess;
-        int _current_card_number;
-        bool _increase_score;
-        int _score;
+        public int _current_card_number;
+        public bool _increase_score;
+        public int _score = 0;
+
 
         ///<summary>
-        ///<summary>
+        /// Checks to see if it is the first turn
+        ///</summary>
         public bool IsFirstTurn()
         {
             return _numDeals == 0;
         }
 
         ///<summary>
-        ///<summary>
+        /// The ResetDeal method handles the clearing of the deal of cards and
+        /// generates the next card in the deal.
+        ///</summary>
         public void ResetDeal()
         {
             _numDeals++;
             _cards.Clear();
             Random randomGenerator = new Random();
             
-            for (int i = 0; i < NUM_CARDS; i++)
-            {
-                int card = randomGenerator.Next(1,14);
-                _cards.Add(card);
-            }
+            int card = randomGenerator.Next(1,14);
+            _cards.Add(card);
         }
 
         ///<summary>
-        ///<summary>
-        public void CheckGuess(string _guess)
+        ///</summary>
+        public void CheckGuess()
         {
             int _numguess;
             if (_guess == "l")
@@ -56,12 +56,12 @@ namespace cse210_tc04
                 if (_numguess < _current_card_number)
                 {
                     _increase_score = true;
-                    CheckScore();
+                    CheckScore(_score);
                 }
-                if (_numguess > _current_card_number)
+                else if (_numguess > _current_card_number)
                 {
                     _increase_score = false;
-                    CheckScore();
+                    CheckScore(_score);
                 }
             }
             if (_guess == "h")
@@ -70,43 +70,55 @@ namespace cse210_tc04
                 if (_numguess > _current_card_number)
                 {
                     _increase_score = true;
-                    CheckScore();
+                    CheckScore(_score);
                 }
-                if (_numguess < _current_card_number)
+                else if (_numguess < _current_card_number)
                 {
                     _increase_score = false;
-                    CheckScore();
+                    CheckScore(_score);
                 }
             }
             
         }
     
         ///<summary>
-        ///<summary>
-        public void CheckScore()
+        ///</summary>
+        public int CheckScore(int _score)
         {
+            
             if (_increase_score == true)
             {
                 _score += 100;
             }
-            if (_increase_score == false)
+            else if (_increase_score == false)
             {
                 _score = _score - 75;
             }
+            return _score;
         }
 
         ///<summary>
-        ///<summary>
-        public void PointsPerCard()
+        /// This function make the score by usingCheckScore function 
+        ///</summary>
+        public int PointsPerCard()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int guessNum = int.Parse(_guess);
+            score += CheckScore(guessNum);
+
+            return score;
         }
 
         ///<summary>
-        ///<summary>
+        /// This function generate next card
+        ///</summary>
         public string DisplayCards()
-        {
-            throw new NotImplementedException();
+        {     
+            Random randomGenerator = new Random();
+            
+            string NextCard = randomGenerator.Next(1,14).ToString();
+
+            return NextCard;
         }
     }
 }
